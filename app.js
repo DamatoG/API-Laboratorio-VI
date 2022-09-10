979322968189f562a0ea101c0711985099b1953b
Gonzalo <gonza.damato@gmail.com>
2022-09-09 21:03:30 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/users', (req, res) => {
+    res.send('Listado de usuarios')
+  })
+
+app.get('/info', (req, res) => {
+    res.sendFile( __dirname + '/public/')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
